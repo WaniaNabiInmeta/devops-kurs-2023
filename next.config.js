33b7d0b38e919361c9bbb18515a2efa049d038b9
WaniaNabiInmeta <wania.nabi@inmeta.no>
2023-09-04 15:07:26 +0200

    standalone

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+  
+    output: "standalone",
+  };
+  
+  module.exports = nextConfig;
